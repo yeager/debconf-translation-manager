d5136abd4f2421df8bfe180a043a8902b5b3af5c
Daniel Nylander <daniel@danielnylander.se>
2026-03-05 16:10:50 +0100

    v0.6.2: Fix startup hang, TX sync 100%

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="debconf-translation-manager",
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(),
     package_data={
         "debconf_translation_manager": [

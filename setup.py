2c38970e2a9045f8b403039084212ca989552014
Daniel Nylander <daniel@danielnylander.se>
2026-03-05 07:26:27 +0100

    Add debian/ packaging for .deb build

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="debconf-translation-manager",
+    version="0.2.0",
+    packages=find_packages(),
+    package_data={
+        "debconf_translation_manager": [
+            "data/*.desktop",
+            "data/icons/hicolor/scalable/apps/*.svg",
+            "locale/*/LC_MESSAGES/*.mo",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "debconf-translation-manager=debconf_translation_manager.__main__:main",
+        ],
+    },
+)

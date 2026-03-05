1ced35e07d546d894af10a80d1b51e784001e4e3
Daniel Nylander <daniel@danielnylander.se>
2026-03-05 07:46:04 +0100

    v0.3.0: Gmail preset, i18n fix, man page po4a, Swedish 100%

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="debconf-translation-manager",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     package_data={
         "debconf_translation_manager": [
